e084a7db39021924f462f673bf3a0b1b938cb821
Mihir Dasgupta <dasguptamihir10@gmail.com>
2021-05-12 18:52:21 +0530

    Function evaluating eq. time for changing maxTau

--- 1dModels/radiative_equilibriumModel/runModel.py
+++ 1dModels/radiative_equilibriumModel/runModel.py
@@ -11,12 +11,12 @@ import csv
 #Importing setup functions for profile development.
 from setupMain import surf_airBdry_tempDiff, radiating_pressure, net_energy_level_in_column, netFlux, heatingRate
 
-#Runs model to equilibrium ~ 260 days.
+# Runs model to equilibrium ~ 260 days for maxTau = 6 [default].
 # Uses net energy in atm. column as stopping criteria.
 
-def runningModel():
+def runningModel(maxTau):
 	#Setting up system components in disequilbrium.
-	diagnostic = climt.Frierson06LongwaveOpticalDepth(linear_optical_depth_parameter=0.1)
+	diagnostic = climt.Frierson06LongwaveOpticalDepth(longwave_optical_depth_at_equator=maxTau, linear_optical_depth_parameter=0.1)
 	radiation = climt.GrayLongwaveRadiation(tendencies_in_diagnostics=True)
 	surface = climt.SlabSurface()
 	albedo = 0.3
@@ -24,6 +24,7 @@ def runningModel():
 	timestep = datetime.timedelta(hours = 4)
 	state = climt.get_default_state([radiation, diagnostic, surface])
 	sw_flux = 200
+	state.update(diagnostic(state))
 	state['downwelling_shortwave_flux_in_air'][:] = sw_flux
 	state['ocean_mixed_layer_thickness'][:] = 1.
 	state['air_temperature'][:] = 200. #Arbitrary init value
@@ -79,16 +80,34 @@ def output_to_csv(timeTaken, olrs, bdry_tempDiff, netEn, surfT, lwFluxNet, lwFlu
 		equilibriumWriter.writerow(str(timeTaken))
 	return 0.
 
+def maxTau_eqTime():
+	maxTaus = [0.5,1,3,6,8,10,12.5,15]
+	out_Time = []
+	for maxT in maxTaus:
+		timeTaken, olrs, bdry_tempDiff, netEn, surfT, lwFluxNet, lwFluxUp, lwFluxDown, heatRate, airTemperatureProf = runningModel(maxT)
+		out_Time.append(timeTaken)
+	out_Time = [float(i.days) for i in out_Time]
+	return out_Time, maxTaus
+
 # runningModel() calls first 3 fns; does not need to be called in main() for runningModel()	
 def main():
-	 timeTaken, olrs, bdry_tempDiff, netEn, surfT, lwFluxNet, lwFluxUp, lwFluxDown, heatRate, airTemperatureProf = runningModel()
-	 output_to_csv(timeTaken, olrs, bdry_tempDiff, netEn, surfT, lwFluxNet, lwFluxUp, lwFluxDown, heatRate, airTemperatureProf)
-
+	maxTau = 6
+	timeTaken, olrs, bdry_tempDiff, netEn, surfT, lwFluxNet, lwFluxUp, lwFluxDown, heatRate, airTemperatureProf = runningModel(maxTau)
+	output_to_csv(timeTaken, olrs, bdry_tempDiff, netEn, surfT, lwFluxNet, lwFluxUp, lwFluxDown, heatRate, airTemperatureProf)
+	outTimes, maxTaus = maxTau_eqTime()
+	plt.xlabel("Aggregate Atmospheric Optimal Thickness")
+	plt.ylabel("Equilibrium Duration (Days)")
+	plt.scatter(maxTaus,outTimes)
+	plt.plot(maxTaus,outTimes,color='black')
+	plt.savefig("../../../graphs_modelling/1dradiative-eq/maxTau_eqTime.png")
+	#plt.scatter()
 if __name__ == '__main__':
 	main()
-# NOTE: DIFF ACCEPTABLE SET TO 5.
 
-"""
+
+""" NOTE: DIFF ACCEPTABLE SET TO 5.
+ ONLY USE radHt after reaching equilbrium. 
+
 Required outs from runningModel():
 	1) olr - historical
 	2) bdry_tempDiff - historical
@@ -103,4 +122,3 @@ Required outs from runningModel():
 	11) equilibrium time
 """
 
-#NOTE: ONLY USE radHt after reaching equilbrium. 
