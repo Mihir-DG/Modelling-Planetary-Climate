c9b29c727c98906ef4469cdbb4ce64c8a2cf0863
Mihir Dasgupta <dasguptamihir10@gmail.com>
2021-06-20 11:52:32 +0530

    potential temp fn +  graph fn added

---
+++ RCEModel/potentialTemperatureProfiles.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import metpy.calc as calc
+from metpy.units import units
+import climt
+
+def potentialTemperature_verticalProfile(state):
+  potentialTemperatures = []
+  for i in range(28):
+    pressure = np.array(state['air_pressure_on_interface_levels']).flatten()[i]
+    pressure  = units.Quantity(pressure,"pascal")
+    temperature = np.array(state['air_temperature']).flatten()[i]
+    temperature = units.Quantity(temperature,"kelvin")
+    potentialTemperatures.append(calc.potential_temperature(pressure,temperature))
+  potentialTemperatures = [i.magnitude for i in potentialTemperatures]
+  
+  # Graphing Theta.
+  plt.plot(np.array(state['air_pressure_on_interface_levels']).flatten()[:28],potentialTemperatures)
+  plt.xlabel("Pressure (Pa)")
+  plt.ylabel("Potential Temperature (K)")
+  plt.gca().invert_xaxis()
+  plt.savefig("graphs/potentialTemperatureProfile.png")
+  plt.close()
+  return potentialTemperatures
